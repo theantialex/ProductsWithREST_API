d85a6b0e2f3fff9432f6a3f1a5fc57e47abc737d
Александра Волынец <saxara21@gmail.com>
2022-06-19 23:58:52 +0300

    Added nodes info handler

--- products_app/api/handlers/nodes.py
+++ products_app/api/handlers/nodes.py
@@ -15,21 +15,59 @@ class NodesView(BaseView):
     @property
     def id(self):
         return str(self.request.match_info.get('id'))
-"""
-    @docs(summary='Получить информацию о категории/продукте')
-    @request_schema(FullItemSchema())
-    async def get(self):
-        query = items_table.select().where(items_table.c.item_id == self.id)
 
-        item = await self.pg.fetchrow(query)
-        if not item:
+    @classmethod
+    def make_item_responce(self, item, children):
+        return {
+            'id': item['item_id'],
+            'name': item['name'],
+            'type': item['type'],
+            'parentId': item['parent_id'],
+            'date': item['date'],
+            'price': item['price_sum'],
+            'children': children
+        }
+
+    @classmethod
+    def make_childfree_item(self, item):
+        return {
+            'id': item['item_id'],
+            'name': item['name'],
+            'type': item['type'],
+            'parentId': item['parent_id'],
+            'date': item['date'],
+            'price': item['price_sum']
+        }
+    
+    async def get_children(self, item):
+        query = items_table.select().distinct(items_table.c.item_id).where(items_table.c.parent_id == item['item_id']) \
+                .order_by(items_table.c.item_id, items_table.c.date.desc())
+        rows = await self.pg.fetch(self.get_sql(query))
+
+        children = []
+        for row in rows:
+            print(self.make_childfree_item(row))
+            children.append(self.make_childfree_item(row))
+            print(children)
+            if row['type'] == 'CATEGORY':
+                children[-1]['children'] = await self.get_children(row)
+
+        if not children:
+            return None
+        return children
+
+
+    @docs(summary='Получить информацию о категории/продукте')
+    async def get(self):
+
+        query = items_table.select().where(items_table.c.item_id == self.id).order_by(items_table.c.date.desc())
+        db_item = await self.pg.fetchrow(self.get_sql(query))
+        if not db_item:
             raise HTTPNotFound(text="Item not found")
         
-        query = select([
-            exists().where(items_table.c.parent_id == self.id)
-        ])
+        print(db_item)
 
-        item['children'] = await self.pg.fetch(query)
+        children = await self.get_children(db_item)
+        item = self.make_item_responce(db_item, children)
 
         return Response(status=HTTPStatus.OK, body=item)
-"""
